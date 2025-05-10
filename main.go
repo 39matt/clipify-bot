8fbd658d3eea95a2442adba3af9a2217c86cd593
39matt <matija.todosijevic.i4@gmail.com>
2025-05-10 16:27:33 +0200

    batman

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	Token string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func main() {
+	dg, err := discordgo.New("Bot " + Token)
+
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	dg.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	c, err := s.State.Channel(m.ChannelID)
+	if err != nil {
+		fmt.Println("error finding channel,", err)
+		return
+	}
+	if c.Name == "kanal" {
+		if m.Content == "hello" {
+			s.ChannelMessageSend(m.ChannelID, "world!")
+		}
+	}
+}
